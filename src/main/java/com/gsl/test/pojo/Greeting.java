47e2a29c000c5c484b0c486c6c725543932d274b
guosenlin <951033414@qq.com>
2017-04-13 03:28:03 +0800

    init

---
+++ src/main/java/com/gsl/test/pojo/Greeting.java
@@ -0,0 +1,23 @@
+package com.gsl.test.pojo;
+
+/**
+ * Created by guosenlin on 4/10/17.
+ */
+public class Greeting {
+
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
